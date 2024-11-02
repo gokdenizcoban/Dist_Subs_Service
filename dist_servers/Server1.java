83e87075f78d3c4c3dc49bfa124b33d3db7dc9d3
Batuhan Yılmaz <98617800+batuhanylmz0@users.noreply.github.com>
2024-11-02 16:17:05 +0300

    Update Server1.java

--- dist_servers/Server1.java
+++ dist_servers/Server1.java
@@ -1,25 +1,18 @@
 import java.io.*;
 import java.net.*;
 
-public class Server3 {
-    private static final int PORT = 5003;
-    private static final int SERVER1_PORT = 5001;
+public class Server1 {
+    private static final int PORT = 5001;
     private static final int SERVER2_PORT = 5002;
-    
+    private static final int SERVER3_PORT = 5003;
+
     public static void main(String[] args) {
         try {
-            // Server3'ün kendi soketi
+            // Server1'in kendi soketi
             ServerSocket serverSocket = new ServerSocket(PORT);
-            System.out.println("Server3 başlatıldı. Port: " + PORT);
+            System.out.println("Server1 başlatıldı. Port: " + PORT);
 
-            // Server1 ve Server2'ye bağlantı kurma girişimi
-            try {
-                Socket socketToServer1 = new Socket("localhost", SERVER1_PORT);
-                System.out.println("Server1'e bağlantı kuruldu.");
-            } catch (ConnectException e) {
-                System.out.println("Server1'e bağlantı kurulamadı. (Henüz başlatılmamış olabilir)");
-            }
-
+            // Server2 ve Server3'e bağlantı kurma girişimi
             try {
                 Socket socketToServer2 = new Socket("localhost", SERVER2_PORT);
                 System.out.println("Server2'ye bağlantı kuruldu.");
@@ -27,11 +20,18 @@ public class Server3 {
                 System.out.println("Server2'ye bağlantı kurulamadı. (Henüz başlatılmamış olabilir)");
             }
 
+            try {
+                Socket socketToServer3 = new Socket("localhost", SERVER3_PORT);
+                System.out.println("Server3'e bağlantı kuruldu.");
+            } catch (ConnectException e) {
+                System.out.println("Server3'e bağlantı kurulamadı. (Henüz başlatılmamış olabilir)");
+            }
+
             // Diğer serverlardan gelecek bağlantıları bekle
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Yeni bağlantı kabul edildi: " + 
-                    clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+                System.out.println("Yeni bağlantı kabul edildi: " +
+                        clientSocket.getInetAddress() + ":" + clientSocket.getPort());
             }
 
         } catch (IOException e) {
